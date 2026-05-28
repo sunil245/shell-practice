50e2c1b122dacdfdd7791a919daf1b5831fdbdcc
sunil thota <sunilthota224@gmail.com>
2026-05-28 10:44:23 +0530

    install mysql

--- install-mysql.sh
+++ install-mysql.sh
@@ -4,32 +4,33 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
  
 if [ $USERID -ne 0 ]
 then
-    echo -e "$R Error::: RUn with root user"
+    echo -e "$R Error::: RUn with root user $N"
     exit 1
 else
-    echo "$G Script started running successfully"
+    echo "$G Script started running successfully $N"
 fi
 
 
 VALIDATE(){
     if [ $1 -eq 0 ]
     then
-        echo "$Y $2 is already installed"
+        echo "$Y $2 is already $N installed"
         exit 1
     else
-        echo "$G $2 is not installed starting to install"
+        echo "$G $2 is not installed starting to install $N"
     fi
 }
 
 dnf list installed mysql
 if [ $? -ne 0 ] 
 then 
-    echo "$Y mysql is not installed go and install"
+    echo "$Y mysql is not installed go and install $N"
     dnf install mysql -y
     VALIDATE $? "MYSQL"
 else
-    echo "$Y Mysql is already installed nothing to do"
+    echo "$Y Mysql is already installed nothing to do $N"
 fi
