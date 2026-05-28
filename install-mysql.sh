643da5becc648c4e9600f63c0cfb59584b5b70c7
sunil thota <sunilthota224@gmail.com>
2026-05-28 09:17:03 +0530

    install mysql

---
+++ install-mysql.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+USERID=$(id -u)
+ 
+if [ $USERID -ne 0 ]
+then
+    echo "Error::: RUn with root user"
+    exit 1
+else
+    echo "Script started running successfully"
+fi
+
+dnf install mysqlhb
+
+if [ $? -eq 0 ]
+then
+    echo "installing mysql is success"
+else
+    echo "installing mysql is failed"
+    exit 1
+fi
